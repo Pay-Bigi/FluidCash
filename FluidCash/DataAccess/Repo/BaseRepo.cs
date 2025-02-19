3f772bb755149c7242e9d58c55355275c09795cb
Victor-Ndulue <ndulue.victor.c@gmail.com>
2025-02-19 06:28:01 +0100

    Completed CUD function repo methods

--- FluidCash/DataAccess/Repo/BaseRepo.cs
+++ FluidCash/DataAccess/Repo/BaseRepo.cs
@@ -6,41 +6,70 @@ namespace FluidCash.DataAccess.Repo;
 
 public sealed class BaseRepo<T> : IBaseRepo<T> where T : class, IBaseEntity
 {
-        private readonly DbSet<T> _dbSet;
+    private readonly DbSet<T> _dbSet;
 
-        public BaseRepo(DataContext dataContext)
-        {
-            _dbSet = dataContext.Set<T>();
-        }
+    public BaseRepo(DataContext dataContext)
+    {
+        _dbSet = dataContext.Set<T>();
+    }
 
-        public IQueryable<T> GetAll()
-        {
-            return _dbSet.AsNoTracking();
-        }
+    public IQueryable<T> GetAll()
+    {
+        return _dbSet.AsNoTracking();
+    }
 
-        public IQueryable<T>
-            GetByCondition
-            (Expression<Func<T, bool>> conditionExpression)
-        {
-            return _dbSet.Where(conditionExpression)
-                .AsNoTracking();
-        }
+    public IQueryable<T>
+        GetByCondition
+        (Expression<Func<T, bool>> conditionExpression)
+    {
+        return _dbSet.Where(conditionExpression)
+            .AsNoTracking();
+    }
 
-        public IQueryable<T>
-            GetAllNonDeleted()
-        {
-            return _dbSet
-                .Where(t => !t.IsDeleted.Value)
-                .AsNoTracking();
-        }
+    public IQueryable<T>
+        GetAllNonDeleted()
+    {
+        return _dbSet
+            .Where(t => !t.IsDeleted.Value)
+            .AsNoTracking();
+    }
 
-        public IQueryable<T>
-            GetNonDeletedByCondition
-            (Expression<Func<T, bool>> condition)
-        {
-            return _dbSet.Where(condition)
-                .Where(t => !t.IsDeleted.Value)
-                .AsNoTracking();
+    public IQueryable<T>
+        GetNonDeletedByCondition
+        (Expression<Func<T, bool>> condition)
+    {
+        return _dbSet.Where(condition)
+            .Where(t => !t.IsDeleted.Value)
+            .AsNoTracking();
 
-        }
+    }
+
+    public async Task 
+        AddAsync(T entity)
+    {
+        await _dbSet.AddAsync(entity);
+    }
+
+    public async Task 
+        AddRangeAsync 
+        (IEnumerable<T> entities)
+    {
+        await _dbSet.AddRangeAsync(entities);
+    }
+
+    public void Update(T entity)
+    {
+        _dbSet.Update(entity);
+    }
+
+    public void 
+        UpdateRange(IEnumerable<T> entities)
+    {
+        _dbSet.UpdateRange(entities);        
+    }
+
+    public void Delete(T entity)
+    {
+        _dbSet.Remove(entity);
+    }
 }
--- FluidCash/DataAccess/Repo/IBaseRepo.cs
+++ FluidCash/DataAccess/Repo/IBaseRepo.cs
@@ -5,11 +5,12 @@ namespace FluidCash.DataAccess.Repo;
 public interface IBaseRepo<T> where T : class
 {
     IQueryable<T> GetAll();
-    IQueryable<T>
-           GetByCondition
-           (Expression<Func<T, bool>> conditionExpression);
+    IQueryable<T> GetByCondition (Expression<Func<T, bool>> conditionExpression);
     IQueryable<T> GetAllNonDeleted();
-    IQueryable<T>
-           GetNonDeletedByCondition
-           (Expression<Func<T, bool>> condition);
+    IQueryable<T> GetNonDeletedByCondition (Expression<Func<T, bool>> condition);
+    Task AddAsync(T entity);
+    Task AddRangeAsync(IEnumerable<T> entities);
+    void Update(T entity);
+    void UpdateRange(IEnumerable<T> entities);
+    void Delete(T entity);
 }
