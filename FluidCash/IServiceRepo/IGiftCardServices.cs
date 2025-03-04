40d37bd11cf64d7d5bcb559c378e7eebac95926d
Victor-Ndulue <ndulue.victor.c@gmail.com>
2025-03-04 18:06:44 +0100

    Added CreateGiftCardAndRateAsync method

--- FluidCash/IServiceRepo/IGiftCardServices.cs
+++ FluidCash/IServiceRepo/IGiftCardServices.cs
@@ -7,26 +7,26 @@ namespace FluidCash.IServiceRepo;
 public interface IGiftCardServices
 {
     Task<StandardResponse<string>> 
-        CreateGiftCardAsync(CreateGiftCardDto createGiftCardDto);
+        CreateGiftCardAsync(CreateGiftCardDto createGiftCardDto, string? userId);
 
     Task<StandardResponse<string>>
-        UpdateGiftCardAsync(UpdateGiftCardDto updateGiftCardDto);
+        UpdateGiftCardAsync(UpdateGiftCardDto updateGiftCardDto, string? userId);
 
     Task<StandardResponse<IEnumerable<GiftCardResponseDto>>>
         GetGiftCardAsync(GetGiftCardDto getGiftCardDto);
 
     Task<StandardResponse<string>>
-        DeleteGiftCardAsync(string giftCardId);
+        DeleteGiftCardAsync(string giftCardId, string? userId);
 
     Task<StandardResponse<string>>
-        CreateGiftCardAndRateAsync(CreateGiftCardAndRateDto createGiftCardAndRateDto);
+        CreateGiftCardAndRateAsync(CreateGiftCardAndRateDto createGiftCardAndRateDto, string? userId);
 
     Task<StandardResponse<string>>
-        UpdateGiftCardRateAsync(UpdateGiftCardRateDto updateGiftCardDto);
+        UpdateGiftCardRateAsync(UpdateGiftCardRateDto updateGiftCardDto, string? userId);
 
     Task<StandardResponse<IEnumerable<GiftCardRateResponseDto>>>
         GetGiftCardRateAsync(GetGiftCardRateDto getGiftCardDto);
 
     Task<StandardResponse<string>>
-        DeleteGiftCardRateAsync(string giftCardId);
+        DeleteGiftCardRateAsync(string giftCardId, string? userId);
 }
--- FluidCash/ServiceRepo/GiftCardServices.cs
+++ FluidCash/ServiceRepo/GiftCardServices.cs
@@ -1,33 +1,69 @@
-﻿using FluidCash.Helpers.ObjectFormatters.DTOs.Requests;
+﻿using FluidCash.DataAccess.Repo;
+using FluidCash.Helpers.ObjectFormatters.DTOs.Requests;
 using FluidCash.Helpers.ObjectFormatters.DTOs.Responses;
 using FluidCash.Helpers.ObjectFormatters.ObjectWrapper;
 using FluidCash.IServiceRepo;
+using FluidCash.Models;
 
 namespace FluidCash.ServiceRepo;
 
 public class GiftCardServices : IGiftCardServices
 {
-    public Task<StandardResponse<string>> CreateGiftCardAndRateAsync(CreateGiftCardAndRateDto createGiftCardAndRateDto)
+    private readonly IBaseRepo<GiftCard> _giftCardRepo;
+    private readonly IBaseRepo<GiftCardRate> _giftCardRateRepo;
+
+    public GiftCardServices(IBaseRepo<GiftCard> giftCardRepo, 
+        IBaseRepo<GiftCardRate> giftCardRateRepo)
+    {
+        _giftCardRepo = giftCardRepo;
+        _giftCardRateRepo = giftCardRateRepo;
+    }
+
+    public async Task<StandardResponse<string>> 
+        CreateGiftCardAndRateAsync
+        (CreateGiftCardAndRateDto createGiftCardAndRateDto, string? userId)
+    {
+        var giftCard = new GiftCard
+        {
+            Category = createGiftCardAndRateDto.category,
+            SubCategory = createGiftCardAndRateDto.subCategory,
+            CreatedBy = userId
+        };
+        var giftCardRate = new GiftCardRate
+        {
+            Rate = createGiftCardAndRateDto.rate,
+            CountryCode = createGiftCardAndRateDto.countryCode,
+            Currency = createGiftCardAndRateDto.currency,
+            GiftCardId = giftCard.Id,
+            CreatedBy = userId
+        };
+
+        await _giftCardRepo.AddAsync(giftCard);
+        await _giftCardRateRepo.AddAsync(giftCardRate);
+
+        await _giftCardRepo.SaveChangesAsync();
+
+        string? message = "Gift Card and Rate created successfully";
+
+        return StandardResponse<string>.Success(message);
+    }
+
+    public Task<StandardResponse<string>> CreateGiftCardAsync(CreateGiftCardDto createGiftCardDto, string? userId)
     {
         throw new NotImplementedException();
     }
 
-    public Task<StandardResponse<string>> CreateGiftCardAsync(CreateGiftCardDto createGiftCardDto)
+    public Task<StandardResponse<string>> DeleteGiftCardAsync(string giftCardId, string? userId)
     {
         throw new NotImplementedException();
     }
 
-    public Task<StandardResponse<string>> DeleteGiftCardAsync(string giftCardId)
+    public Task<StandardResponse<string>> DeleteGiftCardRateAsync(string giftCardId, string? userId)
     {
         throw new NotImplementedException();
     }
 
-    public Task<StandardResponse<string>> DeleteGiftCardRateAsync(string giftCardId)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task<StandardResponse<GiftCardResponseDto>> GetGiftCardAsync(GetGiftCardDto getGiftCardDto)
+    public Task<StandardResponse<IEnumerable<GiftCardResponseDto>>> GetGiftCardAsync(GetGiftCardDto getGiftCardDto)
     {
         throw new NotImplementedException();
     }
@@ -37,17 +73,12 @@ public class GiftCardServices : IGiftCardServices
         throw new NotImplementedException();
     }
 
-    public Task<StandardResponse<string>> UpdateGiftCardAsync(UpdateGiftCardDto updateGiftCardDto)
+    public Task<StandardResponse<string>> UpdateGiftCardAsync(UpdateGiftCardDto updateGiftCardDto, string? userId)
     {
         throw new NotImplementedException();
     }
 
-    public Task<StandardResponse<string>> UpdateGiftCardRateAsync(UpdateGiftCardRateDto updateGiftCardDto)
-    {
-        throw new NotImplementedException();
-    }
-
-    Task<StandardResponse<IEnumerable<GiftCardResponseDto>>> IGiftCardServices.GetGiftCardAsync(GetGiftCardDto getGiftCardDto)
+    public Task<StandardResponse<string>> UpdateGiftCardRateAsync(UpdateGiftCardRateDto updateGiftCardDto, string? userId)
     {
         throw new NotImplementedException();
     }
